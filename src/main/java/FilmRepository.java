d72598ed5f549ee03bee98223ad95311551d3d3d
Anna A <markova29@yandex.ru>
2023-09-19 21:41:03 +0300

    End Change

--- src/main/java/FilmRepository.java
+++ src/main/java/FilmRepository.java
@@ -1,11 +1,12 @@
 public class FilmRepository {
     private int currentFilms;
-    private String nameFilm;
+
     private String[] items = new String[0];
 
     public FilmRepository(int currentFilms) {//изменение кол-ва фильмов
         this.currentFilms = currentFilms;
     }
+
     public FilmRepository() {
         currentFilms = 5;           // по умолчинию кол-во фильмов
     }
@@ -19,22 +20,26 @@ public class FilmRepository {
         tmp[tmp.length - 1] = name;
         items = tmp;
     }
+
     //массив всех фильмов
     public String[] findAll() {
         return items;
     }
+
+
+
     //    массив фильмов с конца
 
     public String[] getItems() {
         int resultLength;
-        if (currentFilms > items.length) {
+        if (currentFilms < items.length) {
             resultLength = items.length;
         } else {
             resultLength = currentFilms;
         }
         String[] tmp = new String[resultLength];
         for (int i = 0; i < items.length; i++) {
-            tmp[i] = items[items.length -1-i];
+            tmp[i] = items[items.length - 1 - i];
         }
         return tmp;
     }
--- src/test/java/FilmRepositoryTest.java
+++ src/test/java/FilmRepositoryTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FilmRepositoryTest {
 
     String item1 = new String("Бладшот");
@@ -10,8 +14,6 @@ public class FilmRepositoryTest {
     String item5 = new String("Человек невидимка");
     String item6 = new String("Тролли. Живой тур");
     String item7 = new String("Номер один");
-
-
 
 // добавление фильма
     @Test
@@ -26,14 +28,12 @@ public class FilmRepositoryTest {
        String[] expected = {item1, item2, item3};
        String[] actual = film.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        assertArrayEquals(expected,actual);
     }
-
-
 
     //вывод всех фильмов
     @Test
-    public void shouldallFilmsTest(){
+    public void shouldAllFilmsTest(){
         FilmRepository film = new FilmRepository();
 
         film.add(item1);
@@ -48,7 +48,42 @@ public class FilmRepositoryTest {
         String[] expected = {item1, item2, item3, item4, item5, item6, item7};
         String[] actual = film.findAll();
 
-        Assertions.assertArrayEquals(expected,actual);
+        assertArrayEquals(expected,actual);
+    }
+// Изменение кол-ва тестов
+    @Test
+    public void shouldChangeAllFilmsTest(){
+        FilmRepository film = new FilmRepository(3);
+
+        film.add(item1);
+        film.add(item2);
+        film.add(item3);
+
+
+
+        String[] expected = {item1, item2, item3};
+        String[] actual = film.findAll();
+
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldAllMaxFilmsTest(){
+        FilmRepository film = new FilmRepository(8);
+
+        film.add(item1);
+        film.add(item2);
+        film.add(item3);
+        film.add(item4);
+        film.add(item5);
+        film.add(item6);
+        film.add(item7);
+
+
+        String[] expected = {item1, item2, item3, item4, item5, item6, item7};
+        String[] actual = film.findAll();
+
+        assertArrayEquals(expected,actual);
     }
 
 //    вывод в обратном порядке
@@ -64,8 +99,25 @@ public class FilmRepositoryTest {
         film.add(item7);
 
         String[] expected = {item7, item6, item5, item4, item3, item2, item1};
-        String[] actual = film.findAll();
+        String[] actual = film.getItems();
 
-        Assertions.assertEquals(expected,actual);
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldAllFindLast(){
+        FilmRepository film = new FilmRepository(7);
+        film.add(item1);
+        film.add(item2);
+        film.add(item3);
+        film.add(item4);
+        film.add(item5);
+        film.add(item6);
+        film.add(item7);
+
+        String[] expected = {item7, item6, item5, item4, item3, item2, item1};
+        String[] actual = film.getItems();
+
+        assertArrayEquals(expected,actual);
     }
 }
